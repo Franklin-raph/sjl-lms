57de52d4b2ed7f637c2ec6d43428550dc86f7fdf
franklin-raph <igboekwulusifranklin@gmail.com>
2023-01-22 14:53:55 +0100

    Update the profile update page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+    const { studentData, isLoading, isSuccess, isError, message } = useSelector(state => state.studentAuth)
+
+    const [first_name, setFirst_name] = useState(studentData ? studentData.user_data.first_name : "")
+    const [last_name, setLast_name] = useState(studentData ? studentData.user_data.last_name : "")
+    const [email, setEmail] = useState(studentData ? studentData.user_data.email : "")
+    const [phone_numuber, setPhone_numuber] = useState("")
+    const [github_repo, setGithub_repo] = useState("")
+    const [linkedln_profile, setLinkedln_profile] = useState("")
 
     const naviate = useNavigate()
-    const { studentData, isLoading, isSuccess, isError, message } = useSelector(state => state.studentAuth)
     useEffect(() => {
         if (studentData === null) {
             naviate('/')
@@ -17,11 +24,15 @@ const Profile = () => {
             <h1 className="text-center text-xl">Profile Update</h1>
             <label className="block my-7">
                 <span className="block text-sm font-medium text-slate-700 pb-2">First Name</span>
-                <input type="text" className="rounded-md py-2 px-2 focus:outline-none text-sm w-full bg-slate-100" />
+                <input type="text" value={first_name} className="rounded-md py-2 px-2 focus:outline-none text-sm w-full bg-slate-100" />
             </label>
             <label className="block my-7">
                 <span className="block text-sm font-medium text-slate-700 pb-2">Last Name</span>
-                <input type="text" className="rounded-md py-2 px-2 focus:outline-none text-sm w-full bg-slate-100" />
+                <input type="text" value={last_name} className="rounded-md py-2 px-2 focus:outline-none text-sm w-full bg-slate-100" />
+            </label>
+            <label className="block my-7">
+                <span className="block text-sm font-medium text-slate-700 pb-2">Email</span>
+                <input type="text" disabled value={email} className="rounded-md py-2 px-2 hover:cursor-not-allowed focus:outline-none text-sm w-full text-slate-400 bg-slate-100" />
             </label>
             <label className="block my-7">
                 <span className="block text-sm font-medium text-slate-700 pb-2">Phone Number</span>
